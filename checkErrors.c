c87066b18a2955b20b30e60b3ab3613558969cd2
Chris Sanchez <csanchez505@gmail.com>
2015-09-24 19:10:49 -0600

    still trying to get check duplicates to work, but no errors exist

--- checkErrors.c
+++ checkErrors.c
@@ -33,12 +33,41 @@ void main(void)
 int checkIfValid(char characterArray[],char twoDimArray[], int inputCharacters)
 {
   int numChars,c,inputRecord;
+  int duplicateIndex;
+  char checkDuplicate[9];
+
   inputRecord = 1;
-  numChars = c = 0;
+  numChars = duplicateIndex = c = 0;
+
   while ((c = getchar()) != EOF)
   {
      printf("%c",c);
+    
+     /* Load 9 elements into this array and see if there are any duplicates */ 
+     checkDuplicate[duplicateIndex] = c;
+    printf("check duplicates: at - %d , %c\n",duplicateIndex, checkDuplicate[duplicateIndex]); 
+     
+    /* we are going to check for duplicates */
+     if(duplicateIndex == 8) 
+     {
 
+       int count = sizeof(checkDuplicate)/sizeof(checkDuplicate[0]);
+       int i,j;
+
+       for (i=0;i<count;i++) 
+       {
+         for(j=i+1;j<count;j++) 
+         {
+           if (checkDuplicate[i] == checkDuplicate[j]) 
+           {
+            // printf("duplicate found\n");
+             inputRecord = NOTVALID;
+           }
+         }
+       }
+       duplicateIndex = 0;
+     }
+     
      if(numChars > MAXCHARACTERS)
      {
          printf("ERROR: TOO MANY : %d\n",numChars);
@@ -53,7 +82,7 @@ int checkIfValid(char characterArray[],char twoDimArray[], int inputCharacters)
          numChars = 0;
          inputRecord = NOTVALID;
        }
-       else if( numChars == MAXCHARACTERS)
+       else if( (numChars == MAXCHARACTERS) && (inputRecord == VALID))
        {
          printf("OK!!!!\n\n");
          numChars = 0;
@@ -61,10 +90,12 @@ int checkIfValid(char characterArray[],char twoDimArray[], int inputCharacters)
        }
      }
     else {
+          /* you want to add characters to elements here */
           inputRecord = VALID;
           numChars++;
-         }
-  }
+         } 
+    duplicateIndex++;
+  }/*end of while loop */
 return 1;
 }
 
